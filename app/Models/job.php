5ca1a3174222d1be716258a2a6f11fca833e9f05
mqaisarkh <muhammadqaisarkhan34@gmail.com>
2025-03-20 15:44:04 +0500

    tags and tagController

--- app/Models/job.php
+++ app/Models/job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class job extends Model
 {
@@ -17,7 +18,7 @@ class job extends Model
         $this->tags()->attach($tag);
     }
 
-    public function tags()
+    public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
     }
